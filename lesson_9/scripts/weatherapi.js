0b500f38482a300c3bf010b775fd4ac54a778b6f
Luis Barboza <46587848+barbozalg@users.noreply.github.com>
2019-03-12 12:59:10 -0400

    Add files via upload

--- lesson_9/scripts/weatherapi.js
+++ lesson_9/scripts/weatherapi.js
@@ -1,6 +1,6 @@
 function gObj(id){ return document.getElementById(id); }
 
-var requestURL = "http://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&appid=2bf68e13904ea713eda41eaaa1672a58";
+var requestURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&appid=2bf68e13904ea713eda41eaaa1672a58";
 var request = new XMLHttpRequest();
 request.open('GET', requestURL);
 request.responseType = 'json';
