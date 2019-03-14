6443a520e163c62163a78a7bf1230de9fc02c3dc
Luis Barboza <46587848+barbozalg@users.noreply.github.com>
2019-03-13 23:48:11 -0400

    Add files via upload

--- lesson_10/scripts/currentdate.js
+++ lesson_10/scripts/currentdate.js
@@ -10,10 +10,22 @@ function weekDayName(num){
         case 0: return "Sunday";
         case 1: return "Monday";
         case 2: return "Tuesday";
-        case 3: return "Wenesday";
+        case 3: return "Wednesday";
         case 4: return "Thursday";
         case 5: return "Friday";
         default: return "Saturday";
+    }
+}
+
+function weekDayN3(num){
+    switch(num){
+        case 0: return "Sun";
+        case 1: return "Mon";
+        case 2: return "Tue";
+        case 3: return "Wed";
+        case 4: return "Thu";
+        case 5: return "Fri";
+        default: return "Sat";
     }
 }
 
@@ -82,8 +94,6 @@ function data(){
     }
 }
 
-function cmplt0(num){
-    return ((num<9)? '0' : '') + num;
-}
+function cmplt0(num){ return ((num<9)? '0' : '') + num; }
 
 gObj("currentDate").innerHTML = weekDayName(d.getDay()) + ", " + d.getDate() + " " + monthName(d.getMonth()) + " " + d.getFullYear();
